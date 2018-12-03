66cf62c33fd06bf4e2a953fc464166fe51ecd62b
Miguel Rocha <miguelantoniooliveirarocha@gmail.com>
2018-12-03 18:16:11 +0000

    computeJointProb done

--- BN.py
+++ BN.py
@@ -11,35 +11,34 @@ class Node():
     def __init__(self, prob, parents = []):
         self.parents = parents
         self.prob = prob
-    
+
+    #Returns a list [a,b], where a is the probability of a node not happening and b is the probability of a node happening
     def computeProb(self, evid):
         parents_size = len(self.parents)
-        print("tamanho: " + str(parents_size))
         parents_sublist = []
         result_list = []
         if(parents_size == 0):
-            result_list.append(1 - self.prob)
-            result_list.append(self.prob)
+            result_list = [1-self.prob, self.prob]
         else:
             for i in range(parents_size):
-                print("Sou o i : " + str(i))
+                if(len(parents_sublist) == 0):
+                    result_list = [1, 0]
                 if(evid[i] == 0 and i != parents_size - 1):
-                    parents_sublist = self.prob[0]
-                if(evid[i] == 0 and i == parents_size - 1):
+                    parents_sublist = self.prob[0]         
+                if(evid[i] == 0 and i == parents_size - 1 and len(parents_sublist) > 0):
                     result_list.append(1 - parents_sublist[0])
                     result_list.append(parents_sublist[0])
                 if(evid[i] == 1 and i != parents_size - 1):
-                    print("oi")
                     parents_sublist = self.prob[1]
-                    print(parents_sublist)
-                if(evid[i] == 1 and i == parents_size - 1):
+                if(evid[i] == 1 and i == parents_size - 1 and len(parents_sublist) >= 2):
                     result_list.append(1 - parents_sublist[1])
                     result_list.append(parents_sublist[1])                   
         return result_list
     
 class BN():
     def __init__(self, gra, prob):
-        pass
+        self.gra = gra
+        self.prob = prob
 
     def computePostProb(self, evid):
         pass
@@ -48,6 +47,11 @@ class BN():
         
         
     def computeJointProb(self, evid):
-        pass
-        
-        return 0
+        prod = 1
+        tamanho = len(self.prob)
+        for i in range(tamanho):
+            if(evid[i] == 0):
+                prod = prod * self.prob[i].computeProb(evid)[0]
+            else:
+                prod = prod * self.prob[i].computeProb(evid)[1]
+        return prod
